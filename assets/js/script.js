985a19e820aa16a81c208aa43a69aebbf4aa9db2
Brandon Hoskins <branjames117@gmail.com>
2021-10-24 21:55:34 -0500

    basic timer functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,28 @@
 console.log('Begin')
+
+// quiz start button
+let quizStartEl = document.querySelector('#quiz-start')
+quizStartEl.addEventListener('click', startQuiz)
+
+function startQuiz() {
+  // initialize the timer
+  let timerEl = document.querySelector('#timer')
+  let timerValue = 60
+  setInterval(() => {
+    timerValue--
+    timerEl.textContent = timerValue
+  }, 1000)
+
+  // remove quiz intro
+  let quizContentEl = document.querySelector('#quiz-content')
+  quizContentEl.remove()
+
+  // begin question loop
+  let mainEl = document.querySelector('main')
+
+  for (let i = 1; i <= 20; i++) {
+    let questionEl = document.createElement('div')
+    questionEl.id = 'quiz-content'
+    mainEl.appendChild(questionEl)
+  }
+}
